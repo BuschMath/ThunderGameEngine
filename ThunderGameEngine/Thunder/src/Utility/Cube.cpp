f98cd0c5831ce830b9d88f3d2d11cab3f1c1482b
BuschMath <busch@iowacentral.edu>
2024-03-26 12:47:51 -0500

    Updated grass block

--- ThunderGameEngine/Thunder/src/Utility/Cube.cpp
+++ ThunderGameEngine/Thunder/src/Utility/Cube.cpp
@@ -3,34 +3,82 @@
 Cube::Cube(GLuint texID)
 {
 	vertices = {
-		glm::vec3{-0.5f, -0.5f, -0.5f},
-		glm::vec3{ 0.5f, -0.5f, -0.5f},  
-		glm::vec3{ 0.5f,  0.5f, -0.5f},  
-		glm::vec3{-0.5f,  0.5f, -0.5f},  
-		glm::vec3{-0.5f, -0.5f,  0.5f},  
-		glm::vec3{ 0.5f, -0.5f,  0.5f},  
-		glm::vec3{ 0.5f,  0.5f,  0.5f},  
-		glm::vec3{-0.5f,  0.5f,  0.5f}  
+		glm::vec3{-1.0f, -1.0f, -1.0f},
+		glm::vec3{ 1.0f, -1.0f, -1.0f},
+		glm::vec3{ 1.0f, -1.0f,  1.0f},
+		glm::vec3{-1.0f, -1.0f,  1.0f},
+		
+		glm::vec3{-1.0f,  1.0f, -1.0f},
+		glm::vec3{ 1.0f,  1.0f, -1.0f},
+		glm::vec3{ 1.0f,  1.0f,  1.0f},
+		glm::vec3{-1.0f,  1.0f,  1.0f},
+
+		glm::vec3{-1.0f, -1.0f,  1.0f},
+		glm::vec3{ 1.0f, -1.0f,  1.0f},
+		glm::vec3{ 1.0f,  1.0f,  1.0f},
+		glm::vec3{-1.0f,  1.0f,  1.0f},
+		
+		glm::vec3{-1.0f, -1.0f, -1.0f},
+		glm::vec3{ 1.0f, -1.0f, -1.0f},
+		glm::vec3{ 1.0f,  1.0f, -1.0f},
+		glm::vec3{-1.0f,  1.0f, -1.0f},
+
+		glm::vec3{-1.0f, -1.0f, -1.0f},
+		glm::vec3{-1.0f, -1.0f,  1.0f},
+		glm::vec3{-1.0f,  1.0f,  1.0f},
+		glm::vec3{-1.0f,  1.0f, -1.0f},
+
+		glm::vec3{ 1.0f, -1.0f, -1.0f},
+		glm::vec3{ 1.0f, -1.0f,  1.0f},
+		glm::vec3{ 1.0f,  1.0f,  1.0f},
+		glm::vec3{1.0f,  1.0f, -1.0f}
 	};
 
 	texCoords = {
-		glm::vec2{0.0f, 0.0f},
-		glm::vec2{1.0f, 0.0f},
-		glm::vec2{1.0f, 1.0f},
-		glm::vec2{0.0f, 1.0f},
-		glm::vec2{0.0f, 0.0f},
-		glm::vec2{1.0f, 0.0f},
-		glm::vec2{1.0f, 1.0f},
-		glm::vec2{0.0f, 1.0f}
+		glm::vec2{0.25f, 0.23f},
+		glm::vec2{0.5f, 0.1f},
+		glm::vec2{0.5f, 0.28f},
+		glm::vec2{0.25f, 0.43f},
+		
+		glm::vec2{0.10f, 0.67f},
+		glm::vec2{0.5f, 0.5f},
+		glm::vec2{0.5f, 0.9f},
+		glm::vec2{0.5f, 0.7f},
+
+		glm::vec2{0.25f, 0.23f},
+		glm::vec2{0.5f, 0.1f},
+		glm::vec2{0.5f, 0.5f},
+		glm::vec2{0.25f, 0.67f},
+
+		glm::vec2{0.25f, 0.23f},
+		glm::vec2{0.5f, 0.1f},
+		glm::vec2{0.5f, 0.5f},
+		glm::vec2{0.25f, 0.67f},
+
+		glm::vec2{0.25f, 0.23f},
+		glm::vec2{0.5f, 0.1f},
+		glm::vec2{0.5f, 0.5f},
+		glm::vec2{0.25f, 0.67f},
+
+		glm::vec2{0.25f, 0.23f},
+		glm::vec2{0.5f, 0.1f},
+		glm::vec2{0.5f, 0.5f},
+		glm::vec2{0.25f, 0.67f},
 	};
 
 	indices = {
-		0, 1, 2,  2, 3, 0,
-		1, 5, 6,  6, 2, 1,
-		7, 6, 5,  5, 4, 7,
-		4, 0, 3,  3, 7, 4,
-		4, 5, 1,  1, 0, 4,
-		3, 2, 6,  6, 7, 3
+		// Bottom Face
+		0, 1, 2,   0, 2, 3,
+		// Top Face
+		4, 5, 6,   4, 6, 7,
+		// Front Face
+		8, 9, 10,  8, 10, 11,
+		// Back Face
+		12, 13, 14,  12, 14, 15,
+		// Left Face
+		16, 17, 18,  16, 18, 19,
+		// Right Face
+		20, 21, 22,  20, 22, 23
 	};
 
 	cubeRender = new RenderComponent(vertices, indices, texCoords, texID);
--- ThunderGameEngine/Thunder/src/main.cpp
+++ ThunderGameEngine/Thunder/src/main.cpp
@@ -3,14 +3,14 @@
 #include "ECS/CameraComponent.h"
 #include "ECS/TransformComponent.h"
 
-char cubeTexturePath[] = "C:/Users/busch/OneDrive - Iowa Central Community College/Computer_Code/ThunderGameEngine/ThunderGameEngine/Thunder/src/Utility/CubeTexture.jpg";
+char cubeTexturePath[] = "C:/Users/busch/OneDrive - Iowa Central Community College/Computer_Code/ThunderGameEngine/ThunderGameEngine/Thunder/src/Utility/GrassDirtCube.jpg";
 
 int main() {
     Engine engine(1600, 1200, "Thunder Game Engine");
 
 	Entity cube = engine.CreateCube(cubeTexturePath);
 
-	Entity camera = engine.CreateCamera(glm::vec3(1, 1, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0), 
+	Entity camera = engine.CreateCamera(glm::vec3(3, 4, 4), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0), 
 		60.0f, 1600.0f / 1200.0f, 0.1f, 100.0f);
 
     engine.Run();
